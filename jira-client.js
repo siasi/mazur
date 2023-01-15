09848104b7232e7f239140b3ecafc2138633dd1b
siasi <2983386+siasi@users.noreply.github.com>
2023-01-15 01:27:53 +0100

    Providing alternative credentials

--- jira-client.js
+++ jira-client.js
@@ -1,28 +1,13 @@
-//import fetch from 'node-fetch'
+import { Version3Client } from 'jira.js';
 
 const JIRA_SERVER="https://boomimagestudio.atlassian.net"
-const JIRA_USER="valentin.popov@boom.co"
-const JIRA_APIKEY="1becJDCckjD7jOvZ7d8p4BF2"
 
-/*fetch(JIRA_SERVER + '/rest/api/3/issue/search?jql=' + `project = ${projectName} AND issuetype = "Epic"`, {
-  method: 'GET',
-  headers: {
-    'Authorization': `Basic ${Buffer.from(
-      JIRA_USER + ':' + JIRA_APIKEY
-    ).toString('base64')}`,
-    'Accept': 'application/json'
-  }
-})
-  .then(response => {
-    console.log(
-      `Response: ${response.status} ${response.statusText}`
-    );
-    return response.text();
-  })
-  .then(text => console.log(text))
-  .catch(err => console.error(err));*/
+//const JIRA_USER="valentin.popov@boom.co"
+//const JIRA_APIKEY="1becJDCckjD7jOvZ7d8p4BF2"
+const JIRA_USER="stefano.iasi@boom.co"
+const JIRA_APIKEY="nPNPbtRIu4w5lLxi74TE0774"
 
-import { Version3Client } from 'jira.js';
+
 const client = new Version3Client({
     host: JIRA_SERVER,
     authentication: {
@@ -34,20 +19,14 @@ const client = new Version3Client({
     newErrorHandling: true,
   });
 
-
-/*const projects = await client.projects.getAllProjects()
-
-if (projects.length) {
-     console.log("Got " + projects.length + " projects:")
-     projects.forEach((project) => console.log(project.name))
-}*/
-
 let projectName =  `"Customer Engagement"`
 const issues = await client.issueSearch.searchForIssuesUsingJql({
       jql: `project = ${projectName} AND issuetype = "Epic"`,
     });
+
     console.log(issues.issues[1]);
     const issue = issues.issues[0];
+
     const row = { 
       epic_id : issue.id,
       key : issue.key,
@@ -59,5 +38,3 @@ const issues = await client.issueSearch.searchForIssuesUsingJql({
       issue_key : '???',
       epic_name : '???'
     }
-    //issues.issues.forEach((issue) => console.log(issue.fields.summary))
-// }
