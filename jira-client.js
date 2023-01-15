31899fad1b917ff84bde19a23b7b9ac044dc0b3a
siasi <2983386+siasi@users.noreply.github.com>
2023-01-15 01:27:53 +0100

    Almost able to get Epic Data

--- jira-client.js
+++ jira-client.js
@@ -77,9 +77,9 @@ function readFromFile(path) {
   return JSON.parse(fs.readFileSync(path, {encoding:'utf8', flag:'r'})) 
 }
 
-function transformIssue(issue, output) {
+function transformIssue(issue, epicIdToName, output) {
 
-  console.log(issue.fields.issuetype.name + " " + issue.key)
+  //console.log(issue.fields.issuetype.name + " " + issue.key)
   if (isATaskOrBug(issue)) {
     var task = newTask(issue);
     output.tasks.push(task)
@@ -97,21 +97,18 @@ function transformIssue(issue, output) {
     //console.log(story)
 
     if (isAClonedStory(issue)) {
-      extractClonedStoriesData(issue, output);
+      transformClonedStoriesData(issue, output);
     }
   } 
-  else if (issue.fields.issuetype.name == 'Epic') {
-    var epic = newEpic(issue);
-    //console.log(epic)
-    output.epics.push(epic);
-  }
 
-  extractSubtasksRelationship(issue, output);
+  transformIssueToEpic(issue, epicIdToName, output)
 
-  extractHistoryData(issue, output);
+  transformSubtasksRelationship(issue, output)
+
+  transformHistoryData(issue, output)
 
   if (hasSprintData(issue)) {
-    extractSprintData(issue, output);
+    transformSprintData(issue, output)
   }         
 }
 
@@ -127,40 +124,66 @@ function hasSprintData(issue) {
   return issue.fields.customfield_10020;
 }
 
-function extractSprintData(issue, output) {
+function transformSprintData(issue, output) {
   for (let s in issue.fields.customfield_10020) {
     var jiraSprint = issue.fields.customfield_10020[s];
-    var sprint = {
-      'issue_id': parseInt(issue.id),
-      'issue_type': issue.fields.issuetype.name,
-      'issue_key': issue.key,
-      'issue_project': "CE",
-      'issue_sprint_id': parseInt(jiraSprint.id),
-      'issue_sprint_name': jiraSprint.name,
-      'issue_sprint_state': jiraSprint.state
-    };
-
-    if (jiraSprint.goal) {
-      sprint.issue_sprint_goal = jiraSprint.goal;
-    }
-
-    if (jiraSprint.startDate) {
-      sprint.issue_sprint_startDate = Date.parse(jiraSprint.startDate);
-    }
-
-    if (jiraSprint.endDate) {
-      sprint.issue_sprint_endDate = Date.parse(sprint.endDate);
-    }
-
-    if (jiraSprint.completeDate) {
-      sprint.issue_sprint_completeDate = Date.parse(sprint.completeDate);
-    }
-
+    var sprint = newIssueToSprint(issue, jiraSprint);
     output.sprints.push(sprint);
   }
 }
 
-function extractHistoryData(issue, output) {
+function newIssueToSprint(issue, jiraSprint) {
+  var issueToSprint = {
+    'issue_id': parseInt(issue.id),
+    'issue_type': issue.fields.issuetype.name,
+    'issue_key': issue.key,
+    'issue_project': "CE",
+    'issue_sprint_id': parseInt(jiraSprint.id),
+    'issue_sprint_name': jiraSprint.name,
+    'issue_sprint_state': jiraSprint.state
+  };
+
+  if (jiraSprint.goal) {
+    issueToSprint.issue_sprint_goal = jiraSprint.goal;
+  }
+
+  if (jiraSprint.startDate) {
+    issueToSprint.issue_sprint_startDate = Date.parse(jiraSprint.startDate);
+  }
+
+  if (jiraSprint.endDate) {
+    issueToSprint.issue_sprint_endDate = Date.parse(issueToSprint.endDate);
+  }
+
+  if (jiraSprint.completeDate) {
+    issueToSprint.issue_sprint_completeDate = Date.parse(issueToSprint.completeDate);
+  }
+  return issueToSprint;
+}
+
+function transformIssueToEpic(issue, epicIdToName, output) {
+  
+  if (issue.fields.parent) {
+    var parent = issue.fields.parent
+    console.log("Epic found " + JSON.stringify(parent, null, 2))
+    if (parent.issuetype.name == "Epic") {
+      
+      var parentId = parseInt(parent.id)
+      var issueToEpic = {
+        id : parseInt(issue.id),
+        key : issue.key,
+        parent_id : parentId,
+        parent_key : parent.key,
+        parent_summary : parent.fields.summary,
+        epic_name : epicIdToName.get(parentId)
+      }
+
+      output.issueToEpic.push(issueToEpic)
+    }
+  }
+}
+
+function transformHistoryData(issue, output) {
   for (let h in issue.changelog.histories) {
     var jiraHistory = issue.changelog.histories[h];
     for (let i in jiraHistory.items) {
@@ -184,7 +207,7 @@ function extractHistoryData(issue, output) {
   }
 }
 
-function extractSubtasksRelationship(issue, output) {
+function transformSubtasksRelationship(issue, output) {
   var subTasks = issue.fields.subtasks;
   for (let s in subTasks) {
     var jiraSubTask = subTasks[s];
@@ -197,19 +220,7 @@ function extractSubtasksRelationship(issue, output) {
   }
 }
 
-function newEpic(issue) {
-  var epic = {
-    'id_epic': parseInt(issue.id),
-    'issue_key': issue.key
-  };
-
-  if (issue.fields.customfield_10011) {
-    epic.epic_name = issue.fields.customfield_10011;
-  }
-  return epic;
-}
-
-function extractClonedStoriesData(issue, output) {
+function transformClonedStoriesData(issue, output) {
   for (let i in issue.fields.issuelinks) {
     var issueLink = issue.fields.issuelinks[i];
     if (issueLink.outwardIssue) {
@@ -282,21 +293,23 @@ function transform(issues) {
     clonedStories : [],
     storyTasks : [],
     historyItems :[],
-    epics : [],
+    issueToEpic : [],
     sprints : []
   }
-  
+
+  var epicIdToName = buildEpicIdToName(issues);
+
   for (let i=0; i<issues.length; i++) {
-    transformIssue(issues[i], tuples)
+    transformIssue(issues[i], epicIdToName, tuples)
   }
   
-  console.log("Found " + tuples.tasks.length + " Tasks/Bugs")
-  console.log("Found " + tuples.stories.length + " Stories")
-  console.log("Found " + tuples.clonedStories.length + " cloned Stories")
-  console.log("Found " + tuples.storyTasks.length + " Subtasks")
-  console.log("Found " + tuples.historyItems.length + " History items")
-  console.log("Found " + tuples.epics.length + " Epic items")
-  console.log("Found " + tuples.sprints.length + " Sprint items")
+  console.log("Created " + tuples.tasks.length + " Tasks/Bugs")
+  console.log("Created " + tuples.stories.length + " Stories")
+  console.log("Created " + tuples.clonedStories.length + " cloned Stories")
+  console.log("Created " + tuples.storyTasks.length + " Subtasks")
+  console.log("Created " + tuples.historyItems.length + " History items")
+  console.log("Created " + tuples.issueToEpic.length + " Issue to Epic items")
+  console.log("Created " + tuples.sprints.length + " Sprint items")
 
   return tuples;
 }
@@ -331,6 +344,21 @@ const db = knex({
     database : 'postgres'
   }
 });
+
+function buildEpicIdToName(issues){
+  var epicIdToName = new Map();
+  for (let i = 0; i < issues.length; i++) {
+    var issue = issues[i];
+    if (issue.fields.issuetype.name == 'Epic') {
+      if (issue.fields.customfield_10011) {
+        epicIdToName.set(parseInt(issue.id), issue.fields.customfield_10011);
+      } else {
+        epicIdToName.set(parseInt(issue.id), "");
+      }
+    }
+  }
+  return epicIdToName
+}
 
 function saveRows(rows, tableName, returningField='id') {
   const chunkSize = 1000;
